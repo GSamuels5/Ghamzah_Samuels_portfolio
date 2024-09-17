b6327da08e09a2912a220ad039c621bb0a527c12
Ghamzah <ghamzahsamuels23@gmail.com>
2024-09-17 13:25:10 +0200

    new portto

--- single-page-dev-portfolio-resource-bundle/starter-template/src/sections/About.tsx
+++ single-page-dev-portfolio-resource-bundle/starter-template/src/sections/About.tsx
@@ -14,9 +14,10 @@ import HtmlIcon from '@/assets/icons/html5.svg'
 import CssIcon from '@/assets/icons/css3.svg'
 import GitICon from '@/assets/icons/github.svg'
 import Chrome from '@/assets/icons/chrome.svg'
-import { TechIcon } from "@/components/Techicon";
 import MapImage from "@/assets/images/Screenshot 2024-09-16 155655.png";
 import SmileImoji from "@/assets/images/Screenshot_2024-09-16_155004-removebg-preview.png"
+import { CardHeader } from "@/components/CardHeader";
+import { ToolboxItems } from "@/components/ToolboxItems";
 
 
 
@@ -24,85 +25,138 @@ const toolboxItems = [
   {
     title: 'C++',
     iconType: CplusIcon,
+    
   },
   {
     title: 'Python',
     iconType: PythonIcon,
+    
   }, {
     title: 'Linux',
     iconType: LinuxIcon,
+  
   }, {
     title: 'MySQL',
     iconType: SqlIcon,
+ 
   }, {
     title: 'Node js',
     iconType: NodeIcon,
+   
   }, {
     title: 'Vue 3',
-    iconType: VueIcon
+    iconType: VueIcon,
+    
   }, {
     title: 'Javascript',
     iconType: JavascriptIcon,
+   
   }, {
     title: 'CSS',
     iconType: CssIcon,
+    
   }, {
     title: 'HTML',
     iconType: HtmlIcon,
+    
   },
   {
     title: 'Github',
-    iconType: GitICon
+    iconType: GitICon,
+   
   } ,{
     title: 'Chrome',
     iconType: Chrome,
   }
 ]
 const skills = [
-  {name: "funny",
-    emoji: 😂
-  }
+  {name: "Communication",
+    emoji: '🗣️',
+    left:'50%',
+    top:'50%',
+  },
+  {name: "Time management",
+    emoji: '⏳',
+    left:'',
+    top:'',
+  },
+  {name: "Networking",
+    emoji: '👥',
+    left:'',
+    top:'',
+  },
+  {name: "Critical Thinking",
+    emoji: '🕵️',
+    left:'',
+    top:'',
+  },
+  {name: "Running",
+    emoji: '🏃',
+    left:'',
+    top:'',
+  },
+  {name: "Reading",
+    emoji: '📖',
+    left:'',
+    top:'',
+  },
+  {name: "Hiking",
+    emoji: '⛰️',
+    left:'',
+    top:'',
+  },
+  {name: "Fitness",
+    emoji: '🏋️‍♀️',
+    left:'',
+    top:'',
+  },
 ]
 export const AboutSection = () => {
   return (
-    <div className="pb-96">
+    <div className="py-20">
+      <div className="container">
   <SectionHeader
   eyebrow="About Me"
 description="Come and explore my journey"
   />
-<div>
-<Card>
-  <div>
-    <StarIcon/>
-    <h3>My Reads</h3>
-    <p>See the books shaping my perspectives</p>
-  </div>
+<div className="mt-20 flex flex-col gap-8">
+<Card className="h-[320px]">
+ <CardHeader title="My Reads" description="See the books shaping my perspectives"/>
+  <div className="w-40 mx-auto mt-8">
 <Image src={bookImage} alt="book cover"/>
-</Card>
-<Card>
-  <div>
-    <StarIcon/>
-    <h3>My Tech Stack</h3>
-    <p>explore my software for developing software.</p>
-  </div>
-<div>
-{toolboxItems.map(item =>(
-  <div key={item.title}> 
-  <TechIcon component={item.iconType}/>
-    <span>{item.title}</span>
-    </div>
-))}
 </div>
 </Card>
-<Card>
-  <StarIcon/>
-  <h3>Soft Skills and Hobbies</h3>
-  <p>Explore my soft skill and hobbies beyond the coding environment.</p>
+<Card className="h-[320px] p-0">
+<CardHeader title="My Tech Stack" 
+description="Explore my software for developing software" className="px-6 pt-6"/>
+ <ToolboxItems items={toolboxItems} className="mt-6" />
+ <ToolboxItems items={toolboxItems} className="mt-6" ItemsWrapperClassName="-translate-x-1/2" /> 
+ </Card>
+<Card className="h-[320px] p-0 flex flex-col ">
+<CardHeader title="Soft Skills and Hobbies" 
+description="Explore my soft skill and hobbies beyond the coding environment"
+className="px-6 py-6"
+/>
+ 
+ 
+  <div className="relative flex-1 ">
+    {skills.map((skill) => (
+      <div key={skill.name} className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 
+      rounded-full py-1.5 absolute" style={{
+        left:skill.left,
+        top: skill.top,
+      }}>
+        <span className="font-medium text-gray-950">{skill.name}</span>
+        <span>{skill.emoji}</span>
+      </div>
+    ))}
+  </div>
 </Card>
 <Card>
 <Image src={MapImage} alt="map"/>
 <Image src={SmileImoji} alt="smilimg memoji"/>
 </Card>
+</div>
 </div>
 </div>
 );
