243525ae851fc70749e31a7454c53fd42ccf379a
Ghamzah <ghamzahsamuels23@gmail.com>
2024-10-04 16:24:14 +0200

    new animation

--- single-page-dev-portfolio-resource-bundle/starter-template/src/sections/Footer.tsx
+++ single-page-dev-portfolio-resource-bundle/starter-template/src/sections/Footer.tsx
@@ -7,13 +7,15 @@ export const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <footer>
+      
   <div className="container">
-    <div>
+    <div className="border-t border-white/15 py-6 flex flex-col items-center
+   md:flex-row md:justify-between text-sm gap-8">
       <div>&copy;{currentYear} <p>All rights reserved </p></div>
-      <nav className="flex flex-col justify-center items-center w-full ">
-        <a href="#"><LinkedinIcon  className="w-[50px] h-[50px]" /></a>
-        <a href="#" ><GithubIcon  className="w-[50px] h-[50px]" /></a>
-        <a href="#" ><CodePenIcon  className="w-[50px] h-[50px]"/></a>
+      <nav className="flex flex-col items-center md:flex-row gap-8">
+        <a href="#" className="inline-flex items-center gap-1.5"><LinkedinIcon  className="w-[50px] h-[50px]" /></a>
+        <a href="#"  className="inline-flex items-center gap-1.5" ><GithubIcon  className="w-[50px] h-[50px]" /></a>
+        <a href="#"  className="inline-flex items-center gap-1.5"><CodePenIcon  className="w-[50px] h-[50px]"/></a>
       </nav>
     </div>
   </div>
