20d49ee6f240793074401e22677156056393da97
Ghamzah <ghamzahsamuels23@gmail.com>
2024-10-09 15:01:38 +0200

    almost complete

--- single-page-dev-portfolio-resource-bundle/starter-template/src/sections/Projects.tsx
+++ single-page-dev-portfolio-resource-bundle/starter-template/src/sections/Projects.tsx
@@ -35,67 +35,70 @@ const portfolioProjects = [
     link: "https://javascript-e-commerce.vercel.app/",
     github: 'https://github.com/GSamuels5/Javascript_E-commerce.git',
     image: EcommercePic,
-  },  
- 
+  },
+
 ];
 
 export const ProjectsSection = () => {
   return (
     <section className="pb-16 lg:py-24">
       <div className="container">
-        <SectionHeader eyebrow="Projects" description="View my design to software transformation"/>
+        <SectionHeader eyebrow="Projects" description="View my design to software transformation" />
         <div className="flex justify-center">
 
-        {/* <h1 className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text text-4xl">Projects</h1> */}
+          {/* <h1 className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text text-4xl">Projects</h1> */}
         </div>
         {/* <p className="font-serif text-3xl md:text-5xl text-center mt-6 pb-2"></p> */}
         <div className="flex flex-col mt-10 gap-20 md:mt-20 ">
-          {portfolioProjects.map((project) => {
+          {portfolioProjects.map((project, projectIndex) => {
             return (
               <Card key={project.title} className="px-8 pt-8 pb-0
-               md:pt-12 md:px-10 lg:pt-16 lg:px-20" >
+               md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky "
+               style={{
+                top: `calc(64px + ${projectIndex * 40}px`
+               }} >
 
-<div className="absolute inset-0 -z-10 opacity-5" style={{
-  backgroundImage: `url(${grainImage.src})`
-}}></div>
-<div className="lg:grid lg:grid-cols-2 lg:gap-16">
-<div className="lg:pb-16">
-                <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex
+                <div className="absolute inset-0 -z-10 opacity-5" style={{
+                  backgroundImage: `url(${grainImage.src})`
+                }}></div>
+                <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                  <div className="lg:pb-16">
+                    <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex
                font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text ">
-                  <span>{project.company}</span>
-                  <span>&bull;</span>
-                  <span>{project.year}</span>
-                </div>
-              
-                <h3 className="font-serif text-2xl mt-2 ">{project.title}</h3>
-                <hr className="border-t-2 border-white/5 mt-4 md:mt-5"/>
-                <p className="flex flex-col gap-4 mt-4 md:mt-5 mb-2">
-                 
-                    
+                      <span>{project.company}</span>
+                      <span>&bull;</span>
+                      <span>{project.year}</span>
+                    </div>
+
+                    <h3 className="font-serif text-2xl mt-2 ">{project.title}</h3>
+                    <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                    <p className="flex flex-col gap-4 mt-4 md:mt-5 mb-2">
+
+
                       {project.results}
-    
-                </p>
-                <div className="flex flex-col md:flex-row md:space-x-2">
-  <a href={project.link} className="w-full md:w-auto ">
-    <button className="bg-white text-gray-950 h-12 w-full md:w-auto rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-4 md:mt-0 px-3">
-      <span>Visit website</span>
-      <ArrowUpIcon className="size-4" />
-    </button>
-  </a>
-  <a href={project.github} className="w-full md:w-auto">
-    <button className="bg-white text-gray-950 h-12 w-full md:w-auto rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-4 md:mt-0 px-3">
-      <span>Github link</span>
-      <ArrowUpIcon className="size-4" />
-    </button>
-  </a>
-</div>
 
-                </div>
-                <div className="relative">
+                    </p>
+                    <div className="flex flex-col md:flex-row md:space-x-2">
+                      <a href={project.link} className="w-full md:w-auto ">
+                        <button className="bg-white text-gray-950 h-12 w-full md:w-auto rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-4 md:mt-0 px-3">
+                          <span>Visit website</span>
+                          <ArrowUpIcon className="size-4" />
+                        </button>
+                      </a>
+                      <a href={project.github} className="w-full md:w-auto">
+                        <button className="bg-white text-gray-950 h-12 w-full md:w-auto rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-4 md:mt-0 px-3">
+                          <span>Github link</span>
+                          <ArrowUpIcon className="size-4" />
+                        </button>
+                      </a>
+                    </div>
 
-                <Image src={project.image} alt={project.title}  className="mt-8 -mb-4  lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none rounded-xl"/>
+                  </div>
+                  <div className="relative">
+
+                    <Image src={project.image} alt={project.title} className="mt-8 -mb-4  lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none rounded-xl" />
+                  </div>
                 </div>
-</div>
               </Card>
             );
           })}
--- single-page-dev-portfolio-resource-bundle/starter-template/src/sections/Testimonials.tsx
+++ single-page-dev-portfolio-resource-bundle/starter-template/src/sections/Testimonials.tsx
@@ -9,6 +9,7 @@ import Image from "next/image";
 import { SectionHeader } from "@/components/SectionHeader";
 import grainImage from "@/assets/images/grain.jpg"
 import { Card } from "@/components/Card";
+import { Fragment } from "react";
 
 const testimonials = [
   {
@@ -53,31 +54,39 @@ const testimonials = [
 
 export const TestimonialsSection = () => {
   return (
-  <div className="py-16 lg:py-24">
-    <div className="container">
-    <SectionHeader eyebrow="Testimonials" description="Don't just take my word for it see what people said about me."/>
-   <div className="mt-16 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%)]">
-    <div className="flex gap-8 flex-none">
-      {testimonials.map(testimonial =>(
-        <Card key={testimonial.name} className="max-w-xs md:max-x-md p-6 md:p-8">
-          <div className="flex gap-4 items-center">
-<div className="size-16  inline-flex items-center justify-center flex-shrink-0">
+    <div className="py-16 lg:py-24">
+      <div className="container">
+        <SectionHeader eyebrow="Testimonials" description="Don't just take my word for it see what people said about me." />
+        <div className="mt-12 lg:mt-20 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%)]
+        py-4 -my-4">
+          <div className="flex gap-8 flex-none
+          animate-move-left [animation-duration:90s]">
+            {[...new Array(2)].fill(0).map((_,index) =>(
+              <Fragment key={index}>
+                          {testimonials.map(testimonial => (
+              <Card key={testimonial.name} className="max-w-xs md:max-x-md p-6 md:p-8
+              hover:rotate-3 transition duration-30s">
+                <div className="flex gap-4 items-center">
+                  <div className="size-16  inline-flex items-center justify-center flex-shrink-0">
 
-<Image src={testimonial.avatar} alt={testimonial.name} className="rounded m-h-full"/>
-</div>
-          <div>
+                    <Image src={testimonial.avatar} alt={testimonial.name} className="rounded m-h-full" />
+                  </div>
+                  <div>
 
-<div className="font-semi-bold">{testimonial.name}</div>
-<div className="text-sm text-white/40">{testimonial.position}</div>
+                    <div className="font-semi-bold">{testimonial.name}</div>
+                    <div className="text-sm text-white/40">{testimonial.position}</div>
+                  </div>
+                </div>
+                <p className="mt-4 md:mt-6 text-sm md:text-base">{testimonial.text}</p>
+
+              </Card>
+            ))}
+              </Fragment>
+            ))}
+  
           </div>
-          </div>
-<p className="mt-4 md:mt-6 text-sm md:text-base">{testimonial.text}</p>
-
-        </Card>
-      ))}
+        </div>
+      </div>
     </div>
-    </div>
-    </div>
-  </div>
   );
 };
