8dfd20cdfdd9eb2a3e947084ad70103258da677a
Ghamzah Samuels <ghamzahsamuels23@gmail.com>
2024-10-24 12:57:38 +0200

    Update layout.tsx

--- single-page-dev-portfolio-resource-bundle/starter-template/src/app/layout.tsx
+++ single-page-dev-portfolio-resource-bundle/starter-template/src/app/layout.tsx
@@ -9,8 +9,8 @@ const calistoga = Calistoga({
   weight: ["400"]
 })
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  title: "Welcome to my Portfolio",
+  description: "This was my first React portfolio",
 };
 
 export default function RootLayout({
